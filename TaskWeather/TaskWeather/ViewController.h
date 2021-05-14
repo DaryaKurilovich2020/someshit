1667a692284959698af5fddf2a19902c0241510d
DaryaKurilovich2020 <71592694+DaryaKurilovich2020@users.noreply.github.com>
2021-05-14 13:09:12 +0300

    Update ViewController.h

--- TaskWeather/TaskWeather/ViewController.h
+++ TaskWeather/TaskWeather/ViewController.h
@@ -8,21 +8,12 @@
 #import <UIKit/UIKit.h>
 
 NSDictionary *temperatureData;
-NSDictionary *windData;
-NSDictionary *weatherData;
-NSDictionary *humidityData;
 
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *citiName;
 
-@property (weak, nonatomic) IBOutlet UILabel *weather;
-
 @property (weak, nonatomic) IBOutlet UILabel *temperature;
-
-@property (weak, nonatomic) IBOutlet UILabel *wind;
-
-@property (weak, nonatomic) IBOutlet UILabel *humidity;
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 
